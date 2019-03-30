52c3013a5a8cb8657828443efc9d89b51fd86a04
Firman Otto <otto.firman23@gmail.com>
2019-03-30 13:43:14 +1100

    Change 10

--- step9.js
+++ step9.js
@@ -1,1 +1,11 @@
-console.log("Hello");
+function buttonAlert(){
+	
+	firstName = document.getElementsByName("firstname")[0];
+	lastName = document.getElementsByName("lastname")[0];
+	
+	
+	alert("" + firstName.value + " " + lastName.value);
+	
+}
+
+
